32c1216630c969a94ea7b94ad36fcd2a3bf857f9
Noob-Lol <111202059+Noob-Lol@users.noreply.github.com>
2023-05-17 17:21:16 +0300

    Create auto lucky.lua

---
+++ auto lucky.lua
@@ -0,0 +1,14 @@
+--put in autoexec folder
+--tp in lucky world for double egg farm
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Noob-Lol/Noob/main/rafa.lua"))()
+local pos = game.Players.LocalPlayer.Character.HumanoidRootPart
+pos.CFrame = CFrame.new(-72,130,1366) 
+local memory = game:GetService("Stats"):GetTotalMemoryUsageMb()
+while true do
+	memory = game:GetService("Stats"):GetTotalMemoryUsageMb()
+	if memory >= 3000 then break --rejoin oh high memory
+	else
+	wait(5)
+	end
+end 
+game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
