4470275e4ef5f209a1bddd0bbaab76297fd18320
Noob-Lol <111202059+Noob-Lol@users.noreply.github.com>
2023-05-18 14:20:06 +0300

    Update AutoLucky.lua

--- AutoLucky.lua
+++ AutoLucky.lua
@@ -1,8 +1,12 @@
 --put in autoexec folder
 --tp in lucky world for double egg farm
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Noob-Lol/Noob/main/rafa.lua"))()
-local pos = game.Players.LocalPlayer.Character.HumanoidRootPart
-pos.CFrame = CFrame.new(-72,130,1366) 
+CurrentWorld = save.World
+if Current world == "Lucky Block World" then
+	local pos = game.Players.LocalPlayer.Character.HumanoidRootPart
+	pos.CFrame = CFrame.new(-72,130,1366) 
+else
+	print("wrong world")
 game.NetworkClient.ChildRemoved:Connect(function() --reconect if disconnected
    game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
 end)
