db8aaf11be58de30dfed316e10b5cd926563fe58
Noob-Lol <111202059+Noob-Lol@users.noreply.github.com>
2023-05-20 21:44:08 +0300

    Update AutoLucky.lua

--- AutoLucky.lua
+++ AutoLucky.lua
@@ -1,20 +1,25 @@
 --put in autoexec folder
 --tp in lucky world for double egg farm
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Noob-Lol/Noob/main/rafa.lua"))()
-local pos = game.Players.LocalPlayer.Character.HumanoidRootPart
-pos.CFrame = CFrame.new(-72,130,1366) 
+if game.PlaceId ==  6284583030 then
+	print("normal")
+	local pos = game.Players.LocalPlayer.Character.HumanoidRootPart
+	pos.CFrame = CFrame.new(-72,130,1366)
+	pos = nil
+elseif game.PlaceId ==  10321372166 then
+	print("hardcore")
+end
 local c =game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("Chat"):FindFirstChildWhichIsA("Frame");c.Active = true;c.Draggable=false
 local ChatSettings = require(game:GetService("Chat").ClientChatModules.ChatSettings)
 ChatSettings.WindowResizable = true --chat resize
-pos = nil
 c = nil
 game.NetworkClient.ChildRemoved:Connect(function() --reconect if disconnected
-   game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
+	game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
 end)
 local memory = nil
 while true do
 	memory = game:GetService("Stats"):GetTotalMemoryUsageMb()
-	if memory >= 3100 then break --rejoin oh high memory
+	if memory >= 3200 then break --rejoin oh high memory
 	else
 	memory = nil
 	wait(20)
